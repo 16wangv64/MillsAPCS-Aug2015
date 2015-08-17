2b74d11cbcaf1039fd38f10e24929a13466176d8
16wangv64 <wangvivian1697@gmail.com>
2015-08-17 08:44:42 -0700

    8/17/15 "Hello World Demonstration"
    The first program was easy. Went smoothly.

---
+++ Vivian Projects/Hello_World.java
@@ -0,0 +1,17 @@
+
+/**
+ * Hello World... This is the first program I've ever written.
+ * 
+ * Vivian Wang 
+ * August 17, 2015
+ */
+public class Hello_World
+{
+    
+    public static void main (String[] args)
+   {
+       System.out.println("Hello World! From Vivian Wang");
+//Display the string
+ 
+   }
+}
