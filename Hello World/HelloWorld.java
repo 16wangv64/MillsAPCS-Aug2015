8197f7907b5a24d2c1ce588402530ac929ca6b24
16wangv64 <wangvivian1697@gmail.com>
2015-08-18 09:21:32 -0700

    Hello There
    Feeling Great

---
+++ Hello World/HelloWorld.java
@@ -0,0 +1,15 @@
+
+/**
+ * This will do a simple println. Our first Java code. 
+ * 
+ * @author Vivian Wang 
+ * @version August 18, 2015
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Vivian");
+        //Display the string
+    }
+}
