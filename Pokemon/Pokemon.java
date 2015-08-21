83eb9e54c9ec88674d231b5edc22f13f76217f8e
16wangv64 <wangvivian1697@gmail.com>
2015-08-21 08:44:39 -0700

    Pokemon
    Pikachu

---
+++ Pokemon/Pokemon.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Pokemon here.
+ * 
+ * @author Vivian 
+ * @version 8/21/15
+ */
+public class Pokemon
+{
+   public static void main (String[]args)   {
+   System.out.println("Pikachu welcomes you to the world of Pokemon!");
+   System.out.println("\t(\\_/)");
+   System.out.println("\t(o^.^)");
+   System.out.println("       z(_(\")(\")");
+      }
+}
+   
+   
